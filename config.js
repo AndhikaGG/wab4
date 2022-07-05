5d232e1db767d5d18dcc051a83a97759c1ef611e
Andhika <78576587+AndhikaGG@users.noreply.github.com>
2022-07-05 23:23:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -34,23 +34,23 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷'
-global.linkgc = 'https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP'
-global.linkig = 'https://www.instagram.com/the.sad.boy01'
+global.WM = '𝗔𝗻𝗱𝗵𝗶𝗸𝗮𝗕𝗼𝘁'
+global.linkgc = 'https://chat.whatsapp.com/KHtibglKt6k7aqODPVMOhB'
+global.linkig = 'https://www.instagram.com/andhiikagg'
 global.deslink = ''
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@the.sad.boy01'
+global.bodlink = '@andhiikagg'
 
-global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
-global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
+global.wait = '_*𝐋𝐨𝐚𝐝𝐢𝐧𝐠 ...*_'
+global.eror = '_*𝐎𝐩𝐬, 𝐒𝐞𝐫𝐯𝐞𝐫 𝐦𝐞𝐧𝐠𝐚𝐥𝐚𝐦𝐢 𝐠𝐚𝐧𝐠𝐠𝐮𝐚𝐧..*_'
 
 //========Url Template Buttons==========//
-global.dtu = '𝑭𝒐𝒍𝒍𝒐𝒘'
-global.urlnya = "https://www.instagram.com/the.sad.boy01"
+global.dtu = '𝗙𝗼𝗹𝗹𝗼𝘄 𝗧𝗶𝗸𝘁𝗼𝗸 𝗸𝘂'
+global.urlnya = "https://tiktok.com/@andhikagg"
 
 //============= callButtons ============//
-global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
+global.dtc = '𝐂𝐚𝐥𝐥 𝐎𝐰𝐧𝐞𝐫'
+global.phn = '+6285749618902'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
